710eb41e7ef16eb7ec26d7f9ace32b761a1634f9
YakSolo <solohinakov@mail.ru>
2024-09-14 03:45:14 +0300

    Initial commit

---
+++ src/main/java/net/yaksolo/ambulance/Ambulance.java
@@ -0,0 +1,15 @@
+package net.yaksolo.ambulance;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Ambulance implements ModInitializer {
+	public static final String MOD_ID = "ambulance";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/yaksolo/ambulance/AmbulanceClient.java
@@ -0,0 +1,10 @@
+package net.yaksolo.ambulance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmbulanceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
